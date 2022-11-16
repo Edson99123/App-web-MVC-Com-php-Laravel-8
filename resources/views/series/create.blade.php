446f6082edb84dba8b6afb18e6d2adb1918dd9ee
Edson Sales <edsonsales99@gmail.com>
2022-11-16 13:46:09 -0400

    Conexçaõ com sqlite, insert de series, listagem de series é redirecionamento de user

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,7 @@
 <x-layout title="Nova Série">
 
-    <form action="" method="POST"> 
+    <form action="/series/salvar" method="post"> 
+        @csrf
         <div class="mb-3" >    
             <label for="nome" class="form-label" >Nome Série:</label>
             <input type="text" id="nome" name="nome" class="form-control">        
